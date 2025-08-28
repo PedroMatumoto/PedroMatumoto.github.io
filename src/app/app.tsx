d488e73db69bd2a8b20ea9a7706da17d1c0cdf16
Pedro Matumoto <pedromatumoto@gmail.com>
2025-08-28 10:54:03 -0300

    feat(nav): implement CardNav component for enhanced navigation

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,79 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/home'
-import { Navbar } from './components/navbar'
+import CardNav from './components/NavCard'
 
 export function App() {
+  const items = [
+    {
+      label: 'About',
+      bgColor: '#0D0716',
+      textColor: '#fff',
+      links: [
+        {
+          label: 'Me',
+          href: '#about',
+          ariaLabel: 'About Me'
+        },
+        {
+          label: 'Experience',
+          href: '#experience',
+          ariaLabel: 'About Experience'
+        }
+      ]
+    },
+    {
+      label: 'Projects',
+      bgColor: '#170D27',
+      textColor: '#fff',
+      links: [
+        {
+          label: 'Featured',
+          href: '#repos',
+          ariaLabel: 'Featured Projects'
+        },
+        {
+          label: 'Case Studies',
+          href: '#case-studies',
+          ariaLabel: 'Project Case Studies'
+        }
+      ]
+    },
+    {
+      label: 'Contact',
+      bgColor: '#271E37',
+      textColor: '#fff',
+      links: [
+        {
+          label: 'Email',
+          href: 'mailto:pedromatumoto@gmail.com',
+          ariaLabel: 'Email us'
+        },
+        {
+          label: 'Github',
+          href: 'https://github.com/PedroMatumoto',
+          ariaLabel: 'Github'
+        },
+        {
+          label: 'LinkedIn',
+          href: 'https://linkedin.com/in/pedro-matumoto/',
+          ariaLabel: 'LinkedIn'
+        }
+      ]
+    }
+  ]
   return (
     <>
       <Router>
-        <Navbar />
+        <CardNav
+          logo="/lotus.svg"
+          logoAlt="Company Logo"
+          items={items}
+          baseColor="#fff"
+          menuColor="#000"
+          buttonBgColor="#111"
+          buttonTextColor="#fff"
+          ease="power3.out"
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<h1>Not Found</h1>} />
